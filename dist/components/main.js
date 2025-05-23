079d6818cb1b79b8221e598563e910690e87e318
Consomeatsky <ibnulaginugas@gmail.com>
2025-05-23 14:55:47 +0700

    Post Pop-Up

--- dist/components/main.js
+++ dist/components/main.js
@@ -263,3 +263,21 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 //   Profile
 
+    document.addEventListener('DOMContentLoaded',() => {
+            const openModalBtn = document.getElementById('openModal');
+            const closeModalBtn = document.getElementById('closeModal');
+            const modal = document.getElementById('modal');
+
+            openModalBtn.addEventListener('click',()=>{
+                modal.classList.remove('hidden');
+            })
+            closeModalBtn.addEventListener('click',()=>{
+                modal.classList.add('hidden');
+            })
+        })
+
+        function hashtag() {
+          const textarea = document.getElementById("text-col");
+          
+          textarea.value += " #";
+        }
